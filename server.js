6be942b503bdf61fa2ec44093a9273ecae5b6732
belanEu <evgeniy.belan.97@mail.ru>
2021-10-01 22:30:18 +0300

    Добавлена проверка на существование файла картинки

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ const routes = require('./src/routes/routes');
 app.use(express.static(config.imagesDir));
 app.use('/', routes);
 
+// todo: handle not found route
+
 app.listen(config.PORT, () => console.log(`server is listening you on port ${config.PORT}`));
--- src/middleware/checkImageExistence.js
+++ src/middleware/checkImageExistence.js
@@ -1,12 +1,18 @@
 const imageStore = require('../services/ImageStore');
+const { existsSync } = require('fs');
+const path = require('path');
+const { imagesDir } = require('../config/config');
 
 module.exports = (req, res, next) => {
     const id = req.params.id; 
     const image = imageStore.findOne(id);
 
     if (image) {
+        if (!existsSync(path.resolve(imagesDir, image.name))) {
+            res.status(404).send('Картинка не найдена :(');
+        }
         next(req, res);
     } else {
-        res.status(404).send('Картинка не найдена :(');
+        res.status(404).send('Данные о картинке не найдены :(');
     }
 };
