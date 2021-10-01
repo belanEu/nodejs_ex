add322833d84c77feeeb6cc1db56fed0dd08a80e
belanEu <evgeniy.belan.97@mail.ru>
2021-10-02 01:51:50 +0300

    Слитие картинок

--- src/controllers/mergeImages.js
+++ src/controllers/mergeImages.js
@@ -1,3 +1,22 @@
+const { replaceBackground } = require('backrem');
+const { createReadStream } = require('fs');
+const imageStore = require('../services/ImageStore');
+const { imagesDir } = require('../config/config');
+const path = require('path');
+    
 module.exports = async(req, res) => {
-    res.sendStatus(200);
+    const query = req.query;
+
+    const frontImageId = query.front, backImageId = query.back,
+    color = query.color.split(',').map(item => +item),
+    threshold = +query.threshold;
+
+    const frontImageFile = path.resolve(imagesDir, imageStore.findOne(frontImageId).name),
+    backImageFile = path.resolve(imagesDir, imageStore.findOne(backImageId).name);
+
+    const frontImageStream = createReadStream(frontImageFile),
+    backImageStream = createReadStream(backImageFile);
+
+    replaceBackground(frontImageStream, backImageStream, color, threshold)
+    .then(readableStream => readableStream.pipe(res));
 };
