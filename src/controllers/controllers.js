2d54abbae4e9338edb64b34dec03f36c1c048284
belanEu <evgeniy.belan.97@mail.ru>
2021-10-02 00:58:22 +0300

    Добавлен контроллер для слития картинок

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -2,11 +2,12 @@ const uploadImage = require('./uploadImage');
 const deleteImageById = require('./deleteImageById');
 const getListOfImages = require('./getListOfImages');
 const downloadImageById = require('./downloadImageById');
+const mergeImages = require('./mergeImages');
 
 module.exports = {
     getListOfImages,
     downloadImageById,
-    // mergeImages: (req, res) => {},
+    mergeImages,
     uploadImage,
     deleteImageById
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,11 +4,12 @@ const router = new express.Router();
 const controller = require('../controllers/controllers');
 const uploadImageService = require('../services/uploadImageService');
 const checkImageExistence = require('../middleware/checkImageExistence');
+const checkValidImages = require('../middleware/checkValidImages');
 
 router.get('/list', controller.getListOfImages);
-router.get('/image/:id', controller.downloadImageById);
+router.get('/image/:id', checkImageExistence, controller.downloadImageById);
 // // /merge?front=<id>&back=<id>&color=145,54,32&threshold=5 
-// router.get('/merge', controller.mergeImages());
+router.get('/merge', checkValidImages, controller.mergeImages);
 
 router.post('/upload', uploadImageService.single('uploaded_file'), controller.uploadImage);
 
